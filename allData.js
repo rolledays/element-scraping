0c353eb6e5f2918c86b794242a27d7b81825a6ef
Aditia <88520567+rolledays@users.noreply.github.com>
2021-11-11 17:02:31 +0700

    allData.js

--- allData.js
+++ allData.js
@@ -21,3 +21,34 @@ function wattpadSearch(query) {
       id.push($(rest).attr('data-id'))
       })
      
+// Detiknews scraper
+axios.get('https://news.detik.com/ajax/mostpopular')
+.then(({ data }) => {
+     const $ = cheerio.load(data.html)
+     let title = []
+     let url = []
+     $('div > h3 > a').get().map((rest) => {
+          title.push($(rest).text())
+     })
+
+     $('div > h3 > a').get().map((rest) => {
+          url.push($(rest).attr('href'))
+     })
+
+// Sindonews scraper
+axios.get(`https://nasional.sindonews.com/`)
+    .then(res => {
+        const $ = scrape.load(res.data);
+        const tag = $('.homelist-new li');
+        tag.each(function(row, i) {
+            data.data[row] = {};
+            const result = $(tag[row]).find('.homelist-title a');
+            const judul = $(tag[row]).find('.homelist-title').text();
+            data.data[row]['link'] = result.attr('href')
+            data.data[row]['judul'] = judul;
+            const waktu = $(tag[row]).find('.homelist-date').text();
+            data.data[row]['waktu'] = waktu;
+            data.data[row]['poster'] = $(tag[row]).find('.homelist-pict a img').attr("src")            
+            data.data[row]['tipe'] = 'Internasional';
+            data.data[row]['kutipan'] = $(tag[row]).find('.homelist-desc').text()
+        });
