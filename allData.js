a3f75216f667fa84ad2d95aa5ef7b6abe1f59ead
Aditia <88520567+rolledays@users.noreply.github.com>
2021-11-11 16:56:48 +0700

    allData.js
    Memek

--- allData.js
+++ allData.js
@@ -1,5 +1,23 @@
 /*Module yang di butuhkan
 const get = require('got')
 const express = require('express')
-const scrape = require('cheerio')
+const cheerio = require('cheerio')
 const axios = require('axios');*/
+
+// Wattpad scraper 
+function wattpadSearch(query) {
+      axios.get('https://www.wattpad.com/search/' + query).then(({ data }) => {
+      const $ = cheerio.load(data)
+      let title = []
+      let url = []
+      let id = []
+      let img = []
+      $('#results-stories > div > ul > li > div > a > div.cover.cover-xs.pull-left > img').get().map((rest) => {
+      title.push($(rest).attr('alt'))
+      img.push($(rest).attr('src'))
+      })
+      $('#results-stories > div > ul > li > div > a').get().map((rest) => {
+      url.push('https://www.wattpad.com' + $(rest).attr('href'))
+      id.push($(rest).attr('data-id'))
+      })
+     
